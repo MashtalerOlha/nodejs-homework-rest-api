6261a570b39f80d30100d5938ff649c05cde8520
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-06-20 16:43:06 +0300

    Update addSchema.js

--- schema/addSchema.js
+++ schema/addSchema.js
@@ -4,8 +4,6 @@ const contactAddSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
-    .min(3)
-    .max(18)
     .pattern(
       /((\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))/
     )
