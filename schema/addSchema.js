ac894df880c30e0fb14acc87142bf0581ab1800b
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-06-20 16:40:49 +0300

    Update addSchema.js

--- schema/addSchema.js
+++ schema/addSchema.js
@@ -1,10 +1,15 @@
 const Joi = require("joi");
 
 const contactAddSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().min(3).max(18).pattern(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/).required(),
-  });
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .min(3)
+    .max(18)
+    .pattern(
+      /((\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))/
+    )
+    .required(),
+});
 
-  module.exports = contactAddSchema;
-  
+module.exports = contactAddSchema;
