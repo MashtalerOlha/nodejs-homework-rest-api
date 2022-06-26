5c08a688943550b95e24df7036e4fd1ace7ec5b4
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-06-26 22:03:43 +0300

    Update getAllContacts.js

--- controllers/getAllContacts.js
+++ controllers/getAllContacts.js
@@ -1,8 +1,8 @@
-const contacts = require("../servise/contacts");
+const { Contact } = require("../models/contact");
 
 const getAll = async (req, res) => {
-    const result = await contacts.listContacts();
-    res.json(result);
+  const result = await Contact.find();
+  res.json(result);
 };
 
 module.exports = getAll;
