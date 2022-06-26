98b86d10a4f3dccf92c67129faf256d514a35d41
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-06-26 22:03:50 +0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,15 @@
-const getAll = require("./getAllContacts")
-const getContactById = require("./getContactById")
+const getAll = require("./getAllContacts");
+const getContactById = require("./getContactById");
 const addContact = require("./addContact");
 const removeContact = require("./removeContact");
 const updateContact = require("./updateContact");
+const updateFavorite = require("./updateFavorite");
 
 module.exports = {
-    getAll,
-    getContactById,
-    addContact,
-    removeContact,
-    updateContact,
-    
+  getAll,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+  updateFavorite,
 };
