bd33c64bc000278715851b38dc78ae5e00a970e7
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-07-03 19:40:55 +0300

    add filter for favorite contact

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,9 +2,9 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const {_id} = req.user;
-  const {page = 1, limit = 5} = req.query;
+  const {page = 1, limit = 5, favorite = true} = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
+  const result = await Contact.find({owner: _id, favorite}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
   res.json(result);
 };
 
