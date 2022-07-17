56f2971a0161465d929a567b58ac22ab9435b366
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-07-17 13:28:27 +0300

    Update sendMail.js

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,19 +1,18 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const {META_PASSWORD} = process.env;
+const { META_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host: "smtp.meta.ua",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "mashtaler887@meta.ua",
-        pass: META_PASSWORD
-    }
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "mashtaler887@meta.ua",
+    pass: META_PASSWORD,
+  },
 };
 
 const transporter = nodemailer.createTransport(nodemailerConfig);
 
-
-    module.exports = transporter
+module.exports = transporter;
